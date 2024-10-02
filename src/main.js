fe473f3e9c5269b5e85079f75270ceddcdf8e318
Joao Matos <joao@tritao.eu>
2024-10-02 15:14:55 +0100

    Fix caching.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+const cache = require('@actions/cache')
 const core = require('@actions/core')
 const tc = require('@actions/tool-cache')
+
+const fs = require('fs')
 const path = require('path')
 
 /**
@@ -15,11 +18,20 @@ async function run() {
       throw new Error('Path must be relative to the workspace')
     }
 
-    // const cachedDir = tc.find('cosmocc', version)
-    // if (cachedDir !== undefined) {
-    //   core.addPath(path.join(cachedDir, 'bin'))
-    //   return
-    // }
+    const cachedDir = tc.find('cosmocc', version)
+    if (fs.existsSync(cachedDir)) {
+      core.addPath(path.join(cachedDir, 'bin'))
+      return
+    }
+
+    const cosmopolitanPath = path.join(process.env.GITHUB_WORKSPACE, userPath)
+
+    const cacheKey = `cosmocc-${version}`
+    const cacheKeyId = await cache.restoreCache([cosmopolitanPath], cacheKey)
+    if (cacheKeyId !== undefined) {
+      core.addPath(path.join(cosmopolitanPath, 'bin'))
+      return
+    }
 
     const urlBase = 'https://cosmo.zip/pub/cosmocc/'
     const url =
@@ -27,10 +39,11 @@ async function run() {
         ? `${urlBase}cosmocc.zip`
         : `${urlBase}cosmocc-${version}.zip`
     const cosmopolitan = await tc.downloadTool(url)
-    const cosmopolitanPath = path.join(process.env.GITHUB_WORKSPACE, userPath)
     await tc.extractZip(cosmopolitan, cosmopolitanPath)
+    core.addPath(path.join(cosmopolitanPath, 'bin'))
+
     const cachedPath = await tc.cacheDir(cosmopolitanPath, 'cosmocc', version)
-    core.addPath(path.join(cachedPath, 'bin'))
+    const cacheId = await cache.saveCache([cosmopolitanPath], cacheKey)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
